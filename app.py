11d40b1538938187de2f969410d3d70c63542535
Ladymapper <mapvisible@gmail.com>
2026-06-30 16:16:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 
+"""
+Security Brief Parser Web Application
+
+This file will contain the Streamlit interface that allows users
+to paste a weekly security brief, process it through the
+Bronze–Silver–Gold pipeline, and download the results.
+
+Implementation will be added after the pipeline has been fully tested.
+"""
