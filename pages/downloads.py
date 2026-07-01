bd092405cdd11950604c3eb9b5302d3936d2b799
Ladymapper <mapvisible@gmail.com>
2026-07-01 16:09:13 +0100

    Update downloads.py

--- pages/downloads.py
+++ pages/downloads.py
@@ -1,8 +1,37 @@
-
 import streamlit as st
+import json
+import pandas as pd
 
 st.title("📥 Downloads")
 
-st.info(
-    "Download Bronze, Silver, Gold, CSV and Excel outputs here."
-)
+if st.session_state.incidents is None:
+
+    st.warning("Nothing to download.")
+
+else:
+
+    incidents = st.session_state.incidents
+    gold = st.session_state.gold
+
+    df = pd.DataFrame(incidents)
+
+    st.download_button(
+        "Download Silver JSON",
+        json.dumps(incidents, indent=4),
+        "silver.json",
+        "application/json"
+    )
+
+    st.download_button(
+        "Download Gold JSON",
+        json.dumps(gold, indent=4),
+        "gold.json",
+        "application/json"
+    )
+
+    st.download_button(
+        "Download CSV",
+        df.to_csv(index=False),
+        "incidents.csv",
+        "text/csv"
+    )
